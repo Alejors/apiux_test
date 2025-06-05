92744c665a6e3bcc4c92f15193042284d8269f0a
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-05 12:14:41 -0500

    added compound unique constraint on books table

--- backend/src/db/migrations/20250605030542-initial-migration.js
+++ backend/src/db/migrations/20250605030542-initial-migration.js
@@ -1,138 +1,146 @@
-'use strict';
+'use strict'
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up (queryInterface, Sequelize) {
     // Authors
     await queryInterface.createTable('authors', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
-        primaryKey: true,
+        primaryKey: true
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false
       },
       createdAt: { type: Sequelize.DATE, allowNull: false },
       updatedAt: { type: Sequelize.DATE, allowNull: false },
-      deletedAt: { type: Sequelize.DATE },
-    });
-    await queryInterface.addIndex('authors', ['name']);
+      deletedAt: { type: Sequelize.DATE }
+    })
+    await queryInterface.addIndex('authors', ['name'])
 
     // Editorials
     await queryInterface.createTable('editorials', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
-        primaryKey: true,
+        primaryKey: true
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false
       },
       createdAt: { type: Sequelize.DATE, allowNull: false },
       updatedAt: { type: Sequelize.DATE, allowNull: false },
-      deletedAt: { type: Sequelize.DATE },
-    });
-    await queryInterface.addIndex('editorials', ['name']);
+      deletedAt: { type: Sequelize.DATE }
+    })
+    await queryInterface.addIndex('editorials', ['name'])
 
     // Genres
     await queryInterface.createTable('genres', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
-        primaryKey: true,
+        primaryKey: true
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false
       },
       createdAt: { type: Sequelize.DATE, allowNull: false },
       updatedAt: { type: Sequelize.DATE, allowNull: false },
-      deletedAt: { type: Sequelize.DATE },
-    });
-    await queryInterface.addIndex('genres', ['name']);
+      deletedAt: { type: Sequelize.DATE }
+    })
+    await queryInterface.addIndex('genres', ['name'])
 
     // Users
     await queryInterface.createTable('users', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
-        primaryKey: true,
+        primaryKey: true
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false
       },
       email: {
         type: Sequelize.STRING,
         allowNull: false,
-        unique: true,
+        unique: true
       },
       password: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false
       },
       createdAt: { type: Sequelize.DATE, allowNull: false },
       updatedAt: { type: Sequelize.DATE, allowNull: false },
-      deletedAt: { type: Sequelize.DATE },
-    });
+      deletedAt: { type: Sequelize.DATE }
+    })
 
     // Books
     await queryInterface.createTable('books', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
-        primaryKey: true,
+        primaryKey: true
       },
       title: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false
       },
       author_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: { model: 'authors', key: 'id' },
         onUpdate: 'CASCADE',
-        onDelete: 'RESTRICT',
+        onDelete: 'RESTRICT'
       },
       editorial_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: { model: 'editorials', key: 'id' },
         onUpdate: 'CASCADE',
-        onDelete: 'RESTRICT',
+        onDelete: 'RESTRICT'
       },
       price: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        allowNull: false
       },
       availability: {
         type: Sequelize.BOOLEAN,
-        allowNull: false,
+        allowNull: false
       },
       genre_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: { model: 'genres', key: 'id' },
         onUpdate: 'CASCADE',
-        onDelete: 'RESTRICT',
+        onDelete: 'RESTRICT'
       },
       createdAt: { type: Sequelize.DATE, allowNull: false },
       updatedAt: { type: Sequelize.DATE, allowNull: false },
-      deletedAt: { type: Sequelize.DATE },
-    });
-    await queryInterface.addIndex('books', ['title']);
-    await queryInterface.addIndex('books', ['price']);
-    await queryInterface.addIndex('books', ['availability']);
+      deletedAt: { type: Sequelize.DATE }
+    })
+    await queryInterface.addIndex(
+      'books',
+      ['title', 'author_id', 'editorial_id'],
+      {
+        unique: true,
+        name: 'unique_book_title_author_editorial'
+      }
+    )
+    await queryInterface.addIndex('books', ['title'])
+    await queryInterface.addIndex('books', ['price'])
+    await queryInterface.addIndex('books', ['availability'])
   },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('books');
-    await queryInterface.dropTable('users');
-    await queryInterface.dropTable('genres');
-    await queryInterface.dropTable('editorials');
-    await queryInterface.dropTable('authors');
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('books')
+    await queryInterface.dropTable('users')
+    await queryInterface.dropTable('genres')
+    await queryInterface.dropTable('editorials')
+    await queryInterface.dropTable('authors')
   }
-};
+}
