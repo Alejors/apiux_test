4aa6a3dd8b8cdd2911c648092a661925783944cd
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-05 04:25:25 -0500

    interface fixes

--- backend/src/modules/books/books.interface.ts
+++ backend/src/modules/books/books.interface.ts
@@ -1,10 +1,11 @@
 import { CreateBookDto, UpdateBookDto } from "./dto/books.dto";
 import { Book } from "./books.entity";
+import { DetailedBook } from "./detailedBook.projection";
 
 export interface IBookRepository {
   create(book: CreateBookDto): Promise<Book>;
-  findAll(): Promise<Book[]>;
-  findOne(filters: object): Promise<Book | null>;
+  findAll(): Promise<DetailedBook[]>;
+  findOne(filters: object): Promise<DetailedBook | null>;
   update(id: number, user: UpdateBookDto): Promise<Book>;
   remove(id: number): Promise<void>;
 }
