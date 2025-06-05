8bc254aac1147b98dffb7e323a6be8af4b2b3210
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-05 02:20:43 -0500

    event sourcing migration

---
+++ backend/src/db/migrations/20250605071701-event_sourcing.js
@@ -0,0 +1,138 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+
+    await queryInterface.createTable('book_events', {
+      book_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'books', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      event_type: {
+        type: Sequelize.ENUM('CREATE', 'UPDATE', 'DELETE'),
+        allowNull: false,
+      },
+      previous_state: {
+        type: Sequelize.JSONB,
+        allowNull: false,
+      },
+      new_state: {
+        type: Sequelize.JSONB,
+        allowNull: false,
+      },
+      createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+    });
+
+    await queryInterface.createTable('editorial_events', {
+      editorial_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'editorials', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      event_type: {
+        type: Sequelize.ENUM('CREATE', 'UPDATE', 'DELETE'),
+        allowNull: false,
+      },
+      previous_state: {
+        type: Sequelize.JSONB,
+        allowNull: false,
+      },
+      new_state: {
+        type: Sequelize.JSONB,
+        allowNull: false,
+      },
+      createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+    });
+
+    await queryInterface.createTable('author_events', {
+      author_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'authors', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      event_type: {
+        type: Sequelize.ENUM('CREATE', 'UPDATE', 'DELETE'),
+        allowNull: false,
+      },
+      previous_state: {
+        type: Sequelize.JSONB,
+        allowNull: false,
+      },
+      new_state: {
+        type: Sequelize.JSONB,
+        allowNull: false,
+      },
+      createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+    });
+
+    await queryInterface.createTable('genre_events', {
+      genre_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'genres', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      event_type: {
+        type: Sequelize.ENUM('CREATE', 'UPDATE', 'DELETE'),
+        allowNull: false,
+      },
+      previous_state: {
+        type: Sequelize.JSONB,
+        allowNull: false,
+      },
+      new_state: {
+        type: Sequelize.JSONB,
+        allowNull: false,
+      },
+      createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('genre_events');
+    await queryInterface.dropTable('author_events');
+    await queryInterface.dropTable('editorial_events');
+    await queryInterface.dropTable('book_events');
+    await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_book_events_event_type";');
+    await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_editorial_events_event_type";');
+    await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_author_events_event_type";');
+    await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_genre_events_event_type";');
+  }
+};
