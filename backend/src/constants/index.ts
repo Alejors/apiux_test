e1aa193d32042a396b162fed161dee78932418fe
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-05 12:18:59 -0500

    book event required constants

--- backend/src/constants/index.ts
+++ backend/src/constants/index.ts
@@ -1,4 +1,6 @@
 export const BOOKS_INTERFACE = "IBooksRepository";
 export const USERS_INTERFACE = "IUsersRepository";
+export const BOOK_EVENT_INTERFACE = "IBookEventsRepository";
+export const BOOK_CREATED_EVENT = "book.created";
 export const SUCCESS_CODE = "success";
 export const FAILED_CODE = "failed";
