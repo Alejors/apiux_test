6e47630fadf74867728a4a4fee72659d7bc5684d
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-06 03:20:22 -0500

    fix on non unicode characters

--- backend/src/common/utils/capitalization.util.ts
+++ backend/src/common/utils/capitalization.util.ts
@@ -1,3 +1,8 @@
 export function capitalizeWords(str: string): string {
-  return str.replace(/\b\w/g, (char) => char.toUpperCase());
+  return str
+    .toLocaleLowerCase()
+    .replace(
+      /\p{L}+/gu,
+      (word) => word.charAt(0).toLocaleUpperCase() + word.slice(1),
+    );
 }
