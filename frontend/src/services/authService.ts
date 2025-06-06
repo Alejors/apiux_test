66fbb9f955407496d8e36c96e37594d3194e89b0
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-06 14:52:26 -0500

    response data typing

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,20 +1,21 @@
 import { apiFetch } from "./requests";
+import type { ApiResponse, User } from "../types/index";
 
-export async function login(email: string, password: string) {
+export async function login(email: string, password: string): Promise<void> {
   return await apiFetch('/auth/login', {
     method: 'POST',
     body: JSON.stringify({ email, password }),
   });
 }
 
-export async function signup(name: string, email: string, password: string) {
+export async function signup(name: string, email: string, password: string): Promise<ApiResponse<User>> {
   return await apiFetch('/auth/register', {
     method: 'POST',
     body: JSON.stringify({ name, email, password }),
   });
 }
 
-export async function logout() {
+export async function logout(): Promise<void> {
   await apiFetch('/auth/logout', { method: 'POST' });
   window.location.href = '/login';
 }
