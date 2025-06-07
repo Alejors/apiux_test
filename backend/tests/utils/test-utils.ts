f633b59a41c7c0f51e143f79ab36dfcf49b1a4d7
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-07 15:02:15 -0500

    test module creationg util

---
+++ backend/tests/utils/test-utils.ts
@@ -0,0 +1,38 @@
+import { Model } from "sequelize-typescript";
+import { ConfigModule } from "@nestjs/config";
+import * as cookieParser from "cookie-parser";
+import { INestApplication } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Test, TestingModule } from "@nestjs/testing";
+
+import jwtConfig from "../../src/config/jwt.config";
+
+export async function createTestModule(
+  entities: (typeof Model)[],
+  imports: any[] = [],
+  providers: any[] = [],
+  controllers: any[] = [],
+) {
+  const moduleRef: TestingModule = await Test.createTestingModule({
+    imports: [
+      ConfigModule.forRoot({ isGlobal: true, load: [jwtConfig] }),
+      SequelizeModule.forRoot({
+        dialect: "sqlite",
+        storage: ":memory:",
+        autoLoadModels: true,
+        synchronize: true,
+        logging: false,
+      }),
+      SequelizeModule.forFeature(entities),
+      ...imports,
+    ],
+    controllers,
+    providers,
+  }).compile();
+
+  const app: INestApplication = moduleRef.createNestApplication();
+  app.use(cookieParser());
+  await app.init();
+
+  return { app, moduleRef };
+}
