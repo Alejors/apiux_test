4c87093c8f9491301b0343c6728f2100410672d3
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-07 15:02:33 -0500

    upload service test

--- backend/src/modules/upload/upload.service.spec.ts
+++ backend/src/modules/upload/upload.service.spec.ts
@@ -1,18 +1,35 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UploadService } from './upload.service';
+import { Test, TestingModule } from "@nestjs/testing";
 
-describe('UploadService', () => {
+import { Upload } from "./upload.interface";
+import { UPLOAD_PROVIDER } from "../../constants/index";
+import { UploadService } from "./upload.service";
+import { createTestModule } from "../../../tests/utils/test-utils";
+
+describe("UploadService", () => {
   let service: UploadService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UploadService],
-    }).compile();
+  const mockUploadProvider: Upload = {
+    upload: jest.fn().mockResolvedValue("https://fake.url/forTesting.jpg"),
+  };
 
-    service = module.get<UploadService>(UploadService);
+  beforeEach(async () => {
+    const { app } = await createTestModule(
+      [],
+      [],
+      [
+        UploadService,
+        { provide: UPLOAD_PROVIDER, useValue: mockUploadProvider },
+      ],
+    );
+
+    service = app.get<UploadService>(UploadService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it("debería retornar la URL de la imagen subida", async () => {
+    const mockBuffer = Buffer.from("Random Info for Testing");
+    const url = await service.upload(mockBuffer);
+
+    expect(url).toBe("https://fake.url/forTesting.jpg");
+    expect(mockUploadProvider.upload).toHaveBeenCalledWith(mockBuffer);
   });
 });
