c6d5623a2546ab0c759a9a0b7e9015265fd5bd7c
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-08 13:20:47 -0500

    guard override option on create test module util

--- backend/tests/utils/test-utils.ts
+++ backend/tests/utils/test-utils.ts
@@ -1,20 +1,22 @@
-import { EventEmitterModule } from "@nestjs/event-emitter";
 import { Model } from "sequelize-typescript";
 import { ConfigModule } from "@nestjs/config";
 import * as cookieParser from "cookie-parser";
-import { INestApplication } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { Test, TestingModule } from "@nestjs/testing";
+import { EventEmitterModule } from "@nestjs/event-emitter";
+import { ExecutionContext, INestApplication } from "@nestjs/common";
 
 import jwtConfig from "../../src/config/jwt.config";
+import { AuthGuard } from "../../src/common/guards/auth.guard";
 
 export async function createTestModule(
   entities: (typeof Model)[],
   imports: any[] = [],
   providers: any[] = [],
   controllers: any[] = [],
+  overrideGuard: boolean = false,
 ) {
-  const moduleRef: TestingModule = await Test.createTestingModule({
+  const moduleBuilder = Test.createTestingModule({
     imports: [
       EventEmitterModule.forRoot(),
       ConfigModule.forRoot({ isGlobal: true, load: [jwtConfig] }),
@@ -30,7 +32,15 @@ export async function createTestModule(
     ],
     controllers,
     providers,
-  }).compile();
+  });
+
+  if (overrideGuard) {
+    moduleBuilder.overrideGuard(AuthGuard).useValue({
+      canActivate: (context: ExecutionContext) => true,
+    });
+  }
+
+  const moduleRef: TestingModule = await moduleBuilder.compile();
 
   const app: INestApplication = moduleRef.createNestApplication();
   app.use(cookieParser());
